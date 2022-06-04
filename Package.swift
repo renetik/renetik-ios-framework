261f36e168df5f61208cecf79a6f12c8c71db466
Rene Dohan <dohan.rene@gmail.com>
2022-06-04 17:51:22 -0500

    ...

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RenetikFramework",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "RenetikFramework",
+            targets: ["RenetikFramework"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/rene-dohan/renetik-library-ios", branch: "master"),
+	.package(url: "https://github.com/rene-dohan/renetik-ios-layout", branch: "master"),
+    ],
+    targets: [
+        .target(
+            name: "RenetikFramework",
+            dependencies: []
+        ),
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
