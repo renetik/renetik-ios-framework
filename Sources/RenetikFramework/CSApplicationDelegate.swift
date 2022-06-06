127c70e69e1db893d529c71725183b665a341c5b
Rene Dohan <dohan.rene@gmail.com>
2022-06-06 14:07:22 -0500

    ..

--- Sources/RenetikFramework/CSApplicationDelegate.swift
+++ Sources/RenetikFramework/CSApplicationDelegate.swift
@@ -6,43 +6,38 @@ public let navigation: CSNavigationController = delegate.navigation
 
 open class CSApplicationDelegate: UIResponder, UIApplicationDelegate {
 
-    public var navigation: CSNavigationController! {
-        didSet {
-            Renetik.navigation = navigation
-        }
-    }
-    
+    public var navigation: CSNavigationController!
     public var window: UIWindow?
-    
+
     // ViewController orientation change not called, but this yes for landscape left/right
     public let orientationChange = event()
-    
+
     @discardableResult
     public func onOrientationChange(function: @escaping Func) -> CSRegistration {
         orientationChange.listen(function: function)
     }
-    
+
     public func application(_ application: UIApplication,
                             _ launchOptions: [UIApplication.LaunchOptionsKey: Any]?,
-                            _ logger2: CSLoggerProtocol = NSCSLogger(),
+                            _ logger: CSLoggerProtocol = NSCSLogger(),
                             navigation: CSNavigationController = CSNavigationController()) {
         delegate = self
-        logger = logger2
+        Renetik.initialize(logger, navigation)
         self.navigation = navigation
     }
-    
+
     public func applicationWillResignActive(_ application: UIApplication) { logInfo(application) }
-    
+
     public func applicationDidEnterBackground(_ application: UIApplication) { logInfo(application) }
-    
+
     public func applicationWillEnterForeground(_ application: UIApplication) { logInfo(application) }
-    
+
     public func applicationDidBecomeActive(_ application: UIApplication) { logInfo(application) }
-    
+
     public func application(_ application: UIApplication,
                             didChangeStatusBarOrientation oldStatusBarOrientation: UIInterfaceOrientation) {
         orientationChange.fire()
     }
-    
+
     public func applicationWillTerminate(_ application: UIApplication) { logInfo(application) }
 }
